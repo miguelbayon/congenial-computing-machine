ff74d245a0b7d9857a0711a11d9928e1d8e1bef8
Miguel Bayon <miguel.bayon@iessanandres.com>
2021-11-14 18:07:08 +0100

    Primera version funcional

---
+++ App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Una aplicacion Java muy sencilla
+ * 
+ * @author Miguel Bayon 
+ * @version 0.1
+ */
+public class App
+{
+    /**
+     * Metodo main
+     */
+    public static void main(String[] params)
+    {
+        System.out.println("Esta es una aplicacion Java muy sencilla!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre);
+    }
+}
